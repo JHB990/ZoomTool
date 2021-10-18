cb24a75ed5fa321b143d2cac058371242575a7bc
JHB990 <jazz.j.h.r@gmail.com>
2021-10-18 03:19:04 -0500

    correcion

--- school.py
+++ school.py
@@ -1,10 +1,15 @@
 import webbrowser
 import datetime 
+#import pyautogui as pytho
+import time
 from pandas import *
 
 schedule_file = ExcelFile("horario.xlsx")
-schedule_dataframe = schedule_file.parse(schedule_file.sheet_names[0])
+schedule_dataframe = schedule_file.parse(schedule_file.sheet_names[0]).fillna(0)
 schedule_dict = schedule_dataframe.to_dict()
+
+
+#print(schedule_dict)
 
 days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
 
@@ -15,7 +20,7 @@ def open_class():
     while i < len(schedule_dict["Hora"]):
         index_day = datetime.datetime.now().weekday()
         week_day = days[index_day]
-        if schedule_dict[week_day][i] == "nan":
+        if schedule_dict[week_day][i] == 0:
             pass
         else:
             for day in schedule_dict.keys():
@@ -24,12 +29,20 @@ def open_class():
                     link_key = "Enlace"+week_day
                     link = schedule_dict[link_key][i]
                     
+                    
                     while started != True:
                         current_time = take_current_time()
                         if current_time == hour:
                             print("Abriendo clases...")
-                            webbrowser.open(link)
+                            brave_path = 'C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe %s --incognito'
+                            webbrowser.get(brave_path).open_new(link)
                             started = True
+                            #time.sleep(8)
+                            #pytho.press('c')
+                            #time.sleep(1)
+                            #pytho.write("Listo la sesion esta iniciada")
+                            #time.sleep(2)
+                            #pytho.press("enter")
                         else:
                             continue
             i += 1
